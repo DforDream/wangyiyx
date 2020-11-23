39a9b76fe49db29dacfdb71cf3f1eee0490bb913
DforDream <1500549325@qq.com>
2020-11-23 16:34:56 +0800

    编辑推荐 热销总榜 tab切换效果

--- dist/js/indexMain.js
+++ dist/js/indexMain.js
@@ -1,22 +1,24 @@
 define(["jquery"], function () {
-    // 加载头部
+    // 加载头部模块
     function loadHeader() {
         $(".loadHeader").load("./header.html");
     }
 
     // banner部分
     function move() {
-        var index = 0;
-        var imgs = null;
-        var btns = null;
-        var timer = null;
+        var index = 0; // 默认显示的图片下标
+        var imgs = null; // 图片
+        var btns = null; // 按钮
+        var timer = null; // 计时器
         if (!imgs) {
             imgs = $(".main_banner_img").find("img");
         }
         if (!btns) {
             btns = $(".main_banner_btn").find("a");
         }
+        // 执行一次动画
         tab();
+        // 自动执行动画
         autoMove();
         function autoMove(){
             timer = setInterval(function () {
@@ -25,15 +27,17 @@ define(["jquery"], function () {
             }, 3000);
         }
         
-        
+        // 运动函数
         function tab() {
             if (index > imgs.length - 1) {
                 index = 0;
             }
+            // 图片和按钮显示设置
             imgs.css("opacity", .2).eq(index).show().siblings().hide().end().animate({"opacity":1},300);
             btns.removeClass("active").eq(index).addClass("active");
         }
 
+        // 小圆点的点击事件
         $(".main_banner_btn").on("click","a",function () {
             clearInterval(timer);
             imgs.css("opacity", .2).eq($(this).index()).show().siblings().hide().end().animate({"opacity":1},300);
@@ -43,6 +47,7 @@ define(["jquery"], function () {
             return false;
         })
 
+        // 上一张和下一张点击按钮实现
         $(".main_banner .main_banner_perv,.main_banner .main_banner_next").click(function () {
             clearInterval(timer);
             if (this.className == "main_banner_perv") {
@@ -57,7 +62,7 @@ define(["jquery"], function () {
             autoMove();
         })
 
-
+        // 鼠标的移入移出事件
         $(".main_banner").mouseenter(function () {
             clearInterval(timer);
         })
@@ -67,7 +72,7 @@ define(["jquery"], function () {
         })
     }
 
-
+    // perv和next按钮的定位位置设定
     function pervNextPosition() {
         var width = (($(window).width() - 1090) / 2) - 60;
         $(".main_banner_perv").css("left", width);
@@ -80,26 +85,57 @@ define(["jquery"], function () {
             url: "../data/index.json",
             success: function (data) {
                 // console.log(data);
-                var arr = data.newItemList;
+                var newsArr = data.newItemList;
                 // console.log(arr);
                 // news 切换数据加载
-                for (var i = 0, leni = arr.length; i < leni; i++){
+                for (var i = 0, leni = newsArr.length; i < leni; i++){
                     $(`<div>
+                    <a href="goodsDesc.html?porduct_id=${newsArr[i].id}">
                     <div>
                       <img
-                        src="${arr[i].listPicUrl}"
+                        src="${newsArr[i].listPicUrl}"
                         alt="">
                     </div>
                     <div>
                       <img
-                        src="${arr[i].scenePicUrl}"
+                        src="${newsArr[i].scenePicUrl}"
                         alt="">
                     </div>
-                    <p>${arr[i].name}</p>
-                    <em>￥${arr[i].retailPrice}</em>
+                    <p>${newsArr[i].name}</p>
+                    <em>￥${newsArr[i].retailPrice}</em>
+                    </a>
                   </div>`).appendTo(".main_news_tab .tab_div");
                 }
 
+                // 编辑推荐部分数据加载
+                var popularArr = data.indexPopularItemVO.popularItemList;
+                // console.log(popularArr);
+                for (var i = 0, leni = popularArr.length; i < leni; i++){
+                    $(`<div>
+                    <a href="goodsDesc.html?porduct_id=${popularArr[i].id}">
+                      <img
+                        src="${popularArr[i].showPicUrl}"
+                        alt="">
+                      <p>${popularArr[i].name}</p>
+                      <em>￥${popularArr[i].retailPrice}</em>&nbsp;&nbsp;<del>${popularArr[i].counterPrice ? popularArr[i].counterPrice : popularArr[i].productPlace}</del>
+                    </a>
+                  </div>`).appendTo(".main_popular_goods .main_popular_goods_width");
+                }
+
+                // 热销总榜部分数据加载
+                var popularTotalList = data.indexPopularItemVO.popularTotalList;
+                // console.log(popularArr);
+                for (var i = 0, leni = popularTotalList.length; i < leni; i++){
+                    $(`<div>
+                    <a href="goodsDesc.html?porduct_id=${popularTotalList[i].id}">
+                      <img
+                        src="${popularTotalList[i].showPicUrl}"
+                        alt="">
+                      <p>${popularTotalList[i].name}</p>
+                      <em>￥${popularTotalList[i].retailPrice}</em>&nbsp;&nbsp;<del>${popularTotalList[i].counterPrice ? popularTotalList[i].counterPrice : popularTotalList[i].productPlace}</del>
+                    </a>
+                  </div>`).appendTo(".main_popular_goods .main_popular_goods_hots");
+                }
 
             },
             error: function (err) {
@@ -109,7 +145,7 @@ define(["jquery"], function () {
     }
 
 
-    //  新品推荐
+    //  新品推荐功能
     function newsTab() {
         var index = 0;
         var timer = setInterval(function () {
@@ -122,6 +158,7 @@ define(["jquery"], function () {
                 index = -1;
             }
         }
+        // perv和next功能实现
         $(".main_news .main_banner_perv,.main_news .main_banner_next").click(function () {
             clearInterval(timer);
             if (this.className == "main_banner_perv") {
@@ -138,6 +175,7 @@ define(["jquery"], function () {
             },5000)
         })
 
+        // 鼠标移入移出事件
         $(".main_news_tab").mouseenter(function () {
             clearInterval(timer);
         }).mouseleave(function () {
@@ -149,6 +187,7 @@ define(["jquery"], function () {
         })
     }
 
+    // 左右两个fixed定位实现
     function fixed() {
         $(window).scroll(function () {
             // console.log($(document).scrollTop());
@@ -156,15 +195,39 @@ define(["jquery"], function () {
             if ($(document).scrollTop() >= 600) {
                 $(".main_fixed_right").removeAttr("style").css({
                     "position": "fixed"
-                })
-                $(".main_fixed_left").removeAttr("style").css({ "left": width,"position": "fixed"});
+                });
+                $(".main_fixed_right").children("div:last-child").show();
+                $(".main_fixed_left").removeAttr("style").css({ "left": width, "position": "fixed" });
             } else {
                 $(".main_fixed_right,.main_fixed_left").removeAttr("style").css({
                     "position": "absolute"
                 })
+                $(".main_fixed_right").children("div:last-child").hide();
             }
+        });
+        $(".main_fixed_right").children("div:last-child").click(function () {
+            var height = $(document).scrollTop();
+            var speed = 30;
+            var timer = null;
+            clearInterval(timer);
+            timer = setInterval(function () {
+                height = height - speed;
+                $(document).scrollTop(height);
+                if ($(document).scrollTop() == 0) {
+                    clearInterval(timer);
+                }
+            }, 15)
         })
     }
+
+    // 编辑推荐 热销总榜 tab切换效果
+    function hotsTab() {
+        $(".main_popular .main_news_title").children("p").click(function () {
+            $(this).addClass("active").siblings().removeClass("active");
+            $(".main_popular_goods").children("div").eq($(this).index() - 1).css("z-index", 10).siblings().css("z-index", 1);
+        })
+    }
+
     
 
 
@@ -177,6 +240,7 @@ define(["jquery"], function () {
         pervNextPosition: pervNextPosition,
         download: download,
         newsTab: newsTab,
-        fixed: fixed
+        fixed: fixed,
+        hotsTab: hotsTab
     }
 })
--- js/index.js
+++ js/index.js
@@ -6,10 +6,11 @@ require.config({
 })
 
 require(["indexMain"], function (indexMain) {
-  indexMain.loadHeader();
-  indexMain.move();
-  indexMain.pervNextPosition();
-  indexMain.download();
-  indexMain.newsTab();
-  indexMain.fixed();
+  indexMain.loadHeader(); // 加载头部模块
+  indexMain.move(); // banner部分
+  indexMain.pervNextPosition(); // perv和next按钮定位
+  indexMain.download(); // 首页数据加载
+  indexMain.newsTab(); // news部分功能
+  indexMain.fixed(); // 首页左右fixed定位
+  indexMain.hotsTab(); // 编辑推荐 热销总榜 tab切换效果
 })
